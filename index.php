34b643c21c5c23b7610e21e99da886c9ed58f570
caren65vi <carenvivianagomezramon@gmail.com>
2026-04-08 18:15:50 -0500

    Remove old style.css and update index.php to link to the new stylesheet

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     
 
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 
     <?php wp_head(); ?> </head>
 </head>
